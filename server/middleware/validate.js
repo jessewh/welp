766d47117ca86e3321516711930c31b5c3df6e9c
Jesse Hardage <j.hardage@pm.me>
2022-08-16 18:59:19 +0100

    added helper function

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -1,18 +1,30 @@
-const validEmail = (email) => {
-  const valid = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}$/; 
-  return email.match(valid) && email.length < 25 && email.length > 6;
+// helpers
+const lengthFilter = (input) => {
+  return input.length < 25 && input.length > 6
 }
 
+// User
+//email
+const validEmail = (email) => {
+  const valid = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}$/; 
+  return email.match(valid) && lengthFilter(email);
+}
+
+// password
 const validPassword = (password) => {
   const lower = /[a-z]/.test(password) ? 1 : 0;
   const upper = /[A-Z]/.test(password) ? 1 : 0;
   const digit = /\d/.test(password) ? 1 : 0;
   const special = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(password) ? 1 : 0;
-  return lower + upper + digit + special >= 4;
+  const res = lower + upper + digit + special;
+  return res >= 4 && lengthFilter(password);
 }
 
+// need to add for other user inputs
+
+// organization
 const validOrgName = (orgName) => {
-  return orgName.length < 25 && orgName.length > 6;
+  return lengthFilter(orgName);
 }
 
 const validOrgType = (orgType) => {
